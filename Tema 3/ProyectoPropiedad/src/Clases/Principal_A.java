02e9aca23e485133672d9f56314d51f6be1ae328
ONII <beehrtbeehrt@gmail.com>
2022-10-21 13:32:27 -0500

    Se añadio explicacion a Principal_A del Proyecto Propiedad

--- Tema 3/ProyectoPropiedad/src/Clases/Principal_A.java
+++ Tema 3/ProyectoPropiedad/src/Clases/Principal_A.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Clases;
-import java.util.Scanner;
+import java.util.Scanner; //    Import de la Funcion Scanner
 /**
  *
  * @author ONII
@@ -11,18 +11,35 @@ import java.util.Scanner;
 public class Principal_A {
         
     public static void main(String[] args){
+        
+        // Instancias de la clase Hija Casa y Apartamento
 
         Casa Casa1 = new Casa("Block", "Centrico", 500.00, 2, 35, "Agua", "Luz", "Juan Alberto,", 45);
         Apartamento Apartamento1 = new Apartamento(25, 450.0, false, 1, "UtilName", "Agua", "Luz", "Luis Angel", 30);
         
-        Scanner ObjIn = new Scanner(System.in);
+        Scanner ObjIn = new Scanner(System.in); //  Instancia de Scanner
+        
+        //  Impresion de la clase Hija Casa
         
         System.out.println("// Casa //");
 
         System.out.print("La casa es suya(true / false): ");
-        Casa1.setPertenecia(ObjIn.nextBoolean());
+        Casa1.setPertenecia(ObjIn.nextBoolean());   // Asignacion del valor introducido por teclado al Atributo "pertenencia"
         
-      
+        /*
+                --- Condicional entre Predial y Renta de Objeto Casa1 ---
+            Si de la casa1 Pertenencia es verdadero
+                Solicita al Usiario el Tamaño de la Propiedad y la asignara al Atributo "size"
+                Solicita al Usuario el precio por M2 y lo asignara al Atributo "costeM2"
+                
+                Imprime El importe a pagar de predial llamando a la Función "pagarPredial" que recibe valor de "costeM2"
+                
+            Pero Si de la casa1 Pertenencia es falso
+                Solicita al Usuario la cantidad de mensualidades por pagar y las asigna a la variable "x"
+                Solicita al Usuario el coste de la renta por mes y lo asigna a la variable "y"
+                Imprime el importe a pagar de la renta llamando a la Función "pagarRenta" que recibe valor de "x" y "y"
+        */      
+        
         if (Casa1.pertenecia == true){
             
             System.out.print("Introduzca el tamaño de la propiedad: ");
@@ -44,22 +61,19 @@ public class Principal_A {
             System.out.println("Importe de la renta: " + Casa1.pagarRenta(x, y));
         }  
             
+        
+        //  Impresion de la clase Hija Apartameto
+        
         System.out.println(" ");
         System.out.println("// Apartamento //");
-        
-        if (Apartamento1.pertenencia == false){
+       
+        System.out.print("Introducir el numero de mensualidades por pagar: ");
+        int a = ObjIn.nextInt();    //  Asigna el valor introducido por teclado a la variable "a"
             
-            System.out.print("Introducir el numero de mensualidades por pagar: ");
-            int a = ObjIn.nextInt();
-            
-            System.out.print("Introducir el coste por mes de la renta: ");
-            Apartamento1.setRenta(ObjIn.nextDouble());
+        System.out.print("Introducir el coste por mes de la renta: ");
+        Apartamento1.setRenta(ObjIn.nextDouble());  //  Asigna el valor introducido por teclado al Atributo "renta"
           
-            System.out.println("Importe de la renta: " + Apartamento1.pagarRenta(a));
-        }
-
-    
-    
+        System.out.println("Importe de la renta: " + Apartamento1.pagarRenta(a));   //  Imprime el importe de la renta llamando a la Función "pagarRenta" que recibe el valor de "a"
         
     }
     
